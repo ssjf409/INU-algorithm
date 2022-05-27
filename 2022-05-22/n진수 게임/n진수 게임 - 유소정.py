234f3efc88d8e65fa36e03f1eb4ba475ab607a72
kaori-killer <sksthwjd@daum.net>
2022-05-27 14:15:44 +0900

    n진수 게임 소스코드

--- 2022-05-22/n진수 게임/n진수 게임 - 유소정.py
+++ 2022-05-22/n진수 게임/n진수 게임 - 유소정.py
@@ -2,30 +2,40 @@
 # 미리 구할 숫자의 갯수 t
 # 게임에 참가하는 인원 m
 # 튜브의 순서 p
+
+# 진수 변환
+def change(n, q):
+    check = {"10": "A", "11": "B", "12": "C", "13": "D", "14": "E", "15": "F"}
+    rev_base = ''
+
+    while n > 0:
+        n, mod = divmod(n, q)
+
+        if str(mod) in check:
+            rev_base +=  check[str(mod)]
+        else:
+            rev_base += str(mod)
+
+    return rev_base[::-1]  # 역순인 진수를 뒤집어 줘야 원래 변환 base가 출력
+
+
 def solution(n, t, m, p):
-    li = []
+    li = ['0']
 
     # n진수 리스트
-    for i in range(0, (t * m) + 1):
-        if n == 2:
-            li += list(bin(i)[2:])
-        elif n == 8:
-            li += list(oct(i)[2:])
-        elif n == 16:
-            li += list(hex(i)[2:])
-        else:
-            li += list(str(i))
+    for i in range(1, (t * m) + 1):
+        li += change(i, n)
 
     # 튜브
     answer = ""
     count = 0
 
     for i in range(p-1, len(li) + 1, m):
-        answer += li[i].upper()
+        answer += li[i]
         count += 1
 
         if count == t:
             return answer
 
 
-print(solution(10, 2, 2, 1))
+print(solution(2, 4, 2, 1))
