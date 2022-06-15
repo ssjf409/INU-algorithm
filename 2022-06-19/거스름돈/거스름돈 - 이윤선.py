39b59249973e941072df1bfe9b1ab023ad932770
Yoonsun222 <profit_sun@naver.com>
2022-06-15 09:18:34 +0900

    틀린코드

--- 2022-06-19/거스름돈/거스름돈 - 이윤선.py
+++ 2022-06-19/거스름돈/거스름돈 - 이윤선.py
@@ -1,7 +1,9 @@
 money = int(input())
 cnt = 0
-if money < 5:
+if money < 5 and money % 2:
     cnt = -1
+elif money < 5:
+    cnt += money // 2
 else:
     for i in range(money//5, 0, -1):
         if (money-5*i) % 2 == 0:
