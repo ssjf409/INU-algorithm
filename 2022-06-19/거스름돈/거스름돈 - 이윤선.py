77fae854cc527dc930682bc0d0a839479481d97e
Yoonsun222 <profit_sun@naver.com>
2022-06-15 09:34:36 +0900

    정답코드라고 커밋 되라구우우우우

--- 2022-06-19/거스름돈/거스름돈 - 이윤선.py
+++ 2022-06-19/거스름돈/거스름돈 - 이윤선.py
@@ -1,9 +1,10 @@
+#정답코드인데 왜커밋이 안되냐고
+
 money = int(input())
 cnt = 0
 if money < 5 and money % 2:
     cnt = -1
-elif money < 5:
-    cnt += money // 2
+elif money < 5:   cnt += money // 2
 else:
     for i in range(money//5, -1, -1):
         if (money-5*i) % 2 == 0:
