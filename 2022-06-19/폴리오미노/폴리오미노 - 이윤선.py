d43156ef985718a2bb6c109a93a7d0878f30aed2
Yoonsun222 <profit_sun@naver.com>
2022-06-15 09:10:42 +0900

    .

---
+++ 2022-06-19/폴리오미노/폴리오미노 - 이윤선.py
@@ -0,0 +1,29 @@
+boards = list(input())
+# print(boards)
+cnt = 0
+answer = ''
+for i in range(len(boards)):
+    if boards[i] == 'X':
+        cnt += 1
+    elif boards[i] == '.':
+        if i == 0 or boards[i-1] == '.':
+            answer += '.'
+            continue
+        if cnt % 2:
+            answer = -1
+            break
+        else:
+            a_cnt = cnt // 4
+            b_cnt = (cnt % 4) // 2
+            answer += ('AAAA' * a_cnt + 'BB' * b_cnt + '.')
+            cnt = 0
+if cnt % 2:
+    answer = -1
+else:
+    a_cnt = cnt // 4
+    b_cnt = (cnt % 4) // 2
+    answer += ('AAAA' * a_cnt + 'BB' * b_cnt)
+    
+    
+print(answer)
+
