561f64484b604919eba434d9a654aef804e7ead9
Yoonsun222 <profit_sun@naver.com>
2022-06-24 14:42:46 +0900

    오예

---
+++ 2022-06-26/회의실 배정/회의실 배정 - 이윤선.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+max_end, cnt = 0, 0
+lst = []
+for i in range(N):
+    start, end = map(int, input().split())
+    lst.append([start,end])
+
+lst.sort(key=lambda x:(x[1],x[0]))
+
+for start, end in lst:
+    if start >= max_end:
+            max_end = end
+            cnt += 1
+
+print(cnt)
