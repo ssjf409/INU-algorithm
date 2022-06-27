1fb999eb6f287771ac0bed397d99c1414ebd65b6
Yoonsun222 <profit_sun@naver.com>
2022-06-27 16:28:19 +0900

    2022-07-03 이윤선

--- 2022-06-26/강의실  배정/강의실  배정 - 이윤선.py
+++ 2022-06-26/강의실  배정/강의실  배정 - 이윤선.py
@@ -1,3 +1,26 @@
+# import sys
+# input = sys.stdin.readline
+
+# N = int(input())
+
+# cnt = 0
+# lst = []
+# for i in range(N):
+#     start, end = map(int, input().split())
+#     lst.append([start,end])
+
+# max_lst = [0]
+# lst.sort()
+# 현재 써져있는 if조건을 만족 안할 시 새로운 강의실 배정
+# for i in range(len(lst)):
+#     if lst[i][0] >= max_lst[0]:
+#         max_lst[0] = lst[i][1]
+#     else:
+#         max_lst.append(lst[i][1])
+#     max_lst.sort()
+
+# print(len(max_lst))
+
 #s에 시작 t에 끝나는 n개의 수업 최소의 강의실 사용
 
 import sys, heapq
