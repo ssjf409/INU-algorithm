77c9387f65a89cfacf878332e711d57dfe088bf8
Yoonsun222 <profit_sun@naver.com>
2022-06-27 16:29:27 +0900

    .

--- 2022-06-26/주유소/주유소 - 이윤선.py
+++ 2022-06-26/주유소/주유소 - 이윤선.py
@@ -9,8 +9,7 @@ cost = 0
 for i in range(N-1):
     if money[i] < min_money:
         min_money = money[i]
-        cost += min_money * roads[i]
-    else:
-        cost += min_money * roads[i]
-
+        
+    cost += min_money * roads[i]
+    
 print(cost)
