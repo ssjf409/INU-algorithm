147856042476546bd63755fff3a31d662a9e7bd3
Yoonsun222 <profit_sun@naver.com>
2022-07-02 15:28:33 +0900

    2022-07-02

--- 2022-07-03/상어 초등학교/상어 초등학교 - 이윤선.py
+++ 2022-07-03/상어 초등학교/상어 초등학교 - 이윤선.py
@@ -20,9 +20,11 @@ dy = [0,0,1,-1]
 #print(board)
 
 #lst = ['4 2 5 1 7', '2 1 9 4 5','5 8 1 4 3',' 1 2 9 3 4','7 2 3 4 8','9 8 4 5 7','6 5 2 3 4','8 4 9 2 1','3 9 2 1 4']
-for _ in range(N**2):
-#for input in lst:
-    temp = list(map(int,input().split()))
+
+lst = ['7 9 3 8 2','5 7 3 8 6','3 5 2 4 9', '9 6 8 3 4','8 5 3 1 6', '6 3 8 5 4','2 6 4 8 7', '1 8 3 4 5','4 7 9 3 8']
+#for _ in range(N**2):
+for input in lst:
+    temp = list(map(int,input.split()))
     favor[temp[0]] = temp[1:]
     max_cnt = -1
     max_zero_cnt = 0
@@ -71,6 +73,7 @@ for _ in range(N**2):
     board[x][y] = temp[0]
 
 
+print(board)
 answer = 0
 for i in range(1, N**2+1):
     xx,yy = students[i]
@@ -80,8 +83,8 @@ for i in range(1, N**2+1):
         y = yy + dy[k]
         if  0 <= x < N and 0 <= y < N and board[x][y] in favor[i]:
             cnt += 1
-    
-    if cnt == 0 or cnt == 1:
+
+    if cnt == 1:
         answer += 1
     elif cnt == 2:
         answer += 10
