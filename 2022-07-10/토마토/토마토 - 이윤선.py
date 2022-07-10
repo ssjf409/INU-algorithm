2f1a669badf85e4a935ece0e991927aa03f1aa74
Yoonsun222 <profit_sun@naver.com>
2022-07-10 19:31:54 +0900

    2022-07-10

---
+++ 2022-07-10/토마토/토마토 - 이윤선.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+M, N = map(int, input().split())
+
+
+board = []
+for i in range(N):
+    board.append(list(map(int,input().split())))
+
+# print(board)
+
+
+# N,M = 4, 6
+# board = [[1, -1, 0, 0, 0, 0], [0, -1, 0, 0, 0, 0], [0, 0, 0, 0, -1, 0], [0, 0, 0, 0, -1, 1]]
+queue = deque()
+
+for i in range(N):
+    for j in range(M):
+        if board[i][j] == 1:
+            queue.append([i,j])
+
+
+dx,dy = [1,-1,0,0], [0,0,1,-1]
+max_answer = 0
+while queue:
+    y, x = queue.popleft()
+    for k in range(4):
+        xx = x + dx[k]
+        yy = y + dy[k]
+        if 0<=xx<M and 0 <= yy < N and board[yy][xx] == 0:
+            board[yy][xx] = board[y][x] + 1
+            queue.append([yy,xx])
+
+
+for i in range(N):
+    if 0 in board:
+        print(-1)
+        break
+    else:
+        max_answer = max(max(board[i]),max_answer)
+
+print(max_answer-1)
