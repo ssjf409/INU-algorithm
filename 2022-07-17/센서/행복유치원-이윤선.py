4132d04ce09462cecf1e18433200a8eea8ac11bd
Yoonsun222 <profit_sun@naver.com>
2022-07-12 00:29:45 +0900

    2022-07-12

--- 2022-07-17/센서/행복유치원-이윤선.py
+++ 2022-07-17/센서/행복유치원-이윤선.py
@@ -1,9 +1,10 @@
-N = int(input())
-K = int(input())
+#센서랑 행복유치원이랑 유사한듯보이는데 센서는 모르겠어서 요것을 먼저 푼다아
+
+
+N, K = map(int, input().split())
 
 lst = list(map(int,input().split()))
 lst.sort()
-
 diff = []
 for i in range(1,len(lst)):
     diff.append(lst[i]-lst[i-1])
